4bbe7f9398994c25c927693623ecfe2849f97fa1
Matt Personal <mattpersonal@Matts-MacBook-Pro.local>
2020-01-20 12:18:39 -0700

    added custom js module - modified css and map.json for buckle images

---
+++ app/code/Madcomputing/CustomJS/view/frontend/web/js/custom.js
@@ -0,0 +1,29 @@
+require([
+        'jquery'
+], function ($) {
+    $(function(){
+		var buckleResult = $.get('/images/buckle-images/map.json',showBuckleImages);
+
+
+
+	});
+
+	function showBuckleImages(buckleImageMap){
+		$('.options-list label').each(function(){
+			var optionLabel = $(this);
+			var nameSpan = $('span', optionLabel).first()
+
+			console.log($.trim(nameSpan.text()));
+
+			var buckle = buckleImageMap[$.trim(nameSpan.text())];
+
+			if(buckle){
+				var imagePath = '/images/buckle-images/' + buckle.filename;
+				optionLabel.append('<img src="' + imagePath + '">');
+				optionLabel.addClass('buckle-option-container')
+			}
+			
+		});
+	}
+});
+
