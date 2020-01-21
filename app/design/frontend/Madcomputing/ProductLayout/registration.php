005782258b3d094982a1f7fce0d6c2073d39704a
Matt Personal <mattpersonal@Matts-MacBook-Pro.local>
2020-01-21 14:57:57 -0700

    added custom theme

---
+++ app/design/frontend/Madcomputing/ProductLayout/registration.php
@@ -0,0 +1,10 @@
+<?php
+ 
+\Magento\Framework\Component\ComponentRegistrar::register(
+ 
+  \Magento\Framework\Component\ComponentRegistrar::THEME,
+ 
+  'frontend/Madcomputing/ProductLayout',
+ 
+  __DIR__
+);
