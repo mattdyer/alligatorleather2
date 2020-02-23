f40c48f7cb4180bee8913ff55870b7534ae2cb28
Matt Dyer <madmatt1220@gmail.com>
2020-02-23 11:26:38 -0700

    show initials image - changed color images to use full image size

--- app/code/Madcomputing/CustomJS/view/frontend/web/js/custom.js
+++ app/code/Madcomputing/CustomJS/view/frontend/web/js/custom.js
@@ -4,24 +4,41 @@ require([
     $(function(){
 		var buckleResult = $.get('/images/buckle-images/map.json',showBuckleImages);
 
-
+		showInitialImage();
 
 	});
 
 	function showBuckleImages(buckleImageMap){
 		$('.options-list label').each(function(){
 			var optionLabel = $(this);
-			var nameSpan = $('span', optionLabel).first()
+			var nameSpan = $('span', optionLabel).first();
 
 			var buckle = buckleImageMap[$.trim(nameSpan.text())];
 
 			if(buckle){
 				var imagePath = '/images/buckle-images/' + buckle.filename;
 				optionLabel.append('<img src="' + imagePath + '">');
-				optionLabel.addClass('buckle-option-container')
+				optionLabel.addClass('buckle-option-container');
 			}
 			
 		});
 	}
+	
+	function showInitialImage(){
+		$('.field > label > span').each(function(){
+			var nameSpan = $(this);
+			
+			if(nameSpan.text() == 'Initials'){
+				
+				var control = nameSpan.closest('.field').find('.control');
+				
+				var imagePath = '/images/initials.jpg';
+				control.append('<img class="initials-option-image" src="' + imagePath + '">');
+				control.addClass('initials-option-container');
+			}
+			
+		});
+	}
+	
 });
 
